ce7b825aba146ccc72d3b122041ef1282ca084f9
Diego Schwartz <dschwa01@gmail.com>
2020-03-08 04:09:11 +0000

    Automatically backed up by Learn

---
+++ tree.js
@@ -0,0 +1,17 @@
+const person ={
+  name: "Terrance Roberts",
+  occupation: {
+    title: "District Manage",
+    yearsHeld: 2
+  },
+  pets: [{
+    kind: "dog",
+    name: "Fiona"
+    },{
+    kind: "cat",
+    name: "Ralph"
+    }
+  ]
+}
+
+console.log(person.occupation.title)
